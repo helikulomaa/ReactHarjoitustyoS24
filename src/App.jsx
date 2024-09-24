5b54ab296435d688827bb3605edbebbfb0af98fc
Heli Kulomaa <heli.kulomaa@gmail.com>
2024-09-24 15:01:11 +0300

    Tyhjä projektin aihio

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <p>Tyhjä pohja</p>
+      </div>
+    </>
+  )
+}
+
+export default App
